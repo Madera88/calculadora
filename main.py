44d317f85a3204c077f00258f3e7d281ba3242ca
Ivan <madera-88@hotmail.com>
2023-04-01 11:03:11 +0200

    muestra numeros, permite operativa basica

--- main.py
+++ main.py
@@ -2,32 +2,100 @@ from tkinter import *
 
 #funciones
 def cargar(num):
-    global dato1
-    global dato2
-    global bot
-    if dato1 == None:
-        dato1 = num
-        print("dato1",dato1)
+    if(resultado.get() == 0):
+        resultado.set(num)
     else:
-        dato2 = num
-        print("dato2",dato2)
-
+        aux = resultado.get()
+        aux = str(aux)+str(num)
+        resultado.set(float(aux))
 
 def sumar():
     global dato1
     global dato2
-    if  (dato1 != None and dato2 != None):
-        resultado.set(dato1+dato2)
-        dato1 = None
-        dato2 = None
+    global simbolo
+    aux = resultado.get()
+    if  (dato1 == None ):
+        dato1=aux
+        simbolo="+"
+        resultado.set(0)
+    else:
+        dato2=aux
 
 def resta():
     global dato1
     global dato2
-    if  (dato1 != None and dato2 != None):
+    global simbolo
+    aux = resultado.get()
+    if  (dato1 == None ):
+        dato1=aux
+        simbolo="-"
+        resultado.set(0)
+    else:
+        dato2=aux
+
+def multiplicacion():
+    global dato1
+    global dato2
+    global simbolo
+    aux = resultado.get()
+    if  (dato1 == None ):
+        dato1=aux
+        simbolo="*"
+        resultado.set(0)
+    else:
+        dato2=aux
+
+def division():
+    global dato1
+    global dato2
+    global simbolo
+    aux = resultado.get()
+    if  (dato1 == None ):
+        dato1=aux
+        simbolo="/"
+        resultado.set(0)
+    else:
+        dato2=aux
+
+def resultadoOperacion():
+
+    global dato1
+    global dato2
+    global simbolo
+    global resultado
+    
+    if( simbolo =="+"):
+        sumar()
+        resultado.set(dato1+dato2)
+        dato1=None
+        dato2=None
+    elif( simbolo =="-"):
+        resta()
         resultado.set(dato1-dato2)
-        dato1 = None
-        dato2 = None
+        dato1=None
+        dato2=None
+    elif( simbolo =="*"):
+        resta()
+        resultado.set(dato1*dato2)
+        dato1=None
+        dato2=None
+    elif( simbolo =="/"):
+        resta()
+        resultado.set(dato1/dato2)
+        dato1=None
+        dato2=None
+
+def limpiar():
+    global dato1
+    global dato2
+    global simbolo
+    global resultado
+    dato1 = None
+    dato2 = None
+    simbolo = None
+    resultado.set(0)
+
+    
 
 # creamos la ventana
 ventana = Tk()
@@ -43,6 +111,7 @@ ventana.title("Mi Calculadora")
 resultado=IntVar()
 dato1 = None
 dato2 = None
+simbolo = None
 
 display=Label(ventana , width=26, textvariable=resultado)
 display.config(
@@ -56,6 +125,7 @@ display.config(
 display.grid(column=0, row=0,columnspan=3, padx=5, pady=5)
 Label(ventana).grid(row=1)
 
+#FILA BOTONES 1
 bot = Button(ventana, width=15, text=1, command= lambda: cargar(1))
 bot.grid(padx=5, pady=5, row=2, column=0)
 bot = Button(ventana, width=15, text=2, command= lambda: cargar(2))
@@ -63,6 +133,7 @@ bot.grid(padx=5, pady=5, row=2, column=1)
 bot = Button(ventana, width=15, text=3, command= lambda: cargar(3))
 bot.grid(padx=5, pady=5, row=2, column=2)
 
+#FILA BOTONES 2
 bot = Button(ventana, width=15, text=4, command= lambda: cargar(4))
 bot.grid(padx=5, pady=5, row=3, column=0)
 bot = Button(ventana, width=15, text=5, command= lambda: cargar(5))
@@ -70,6 +141,7 @@ bot.grid(padx=5, pady=5, row=3, column=1)
 bot = Button(ventana, width=15, text=6, command= lambda: cargar(6))
 bot.grid(padx=5, pady=5, row=3, column=2)
 
+#FILA BOTONES 3
 bot = Button(ventana, width=15, text=7, command= lambda: cargar(7))
 bot.grid(padx=5, pady=5, row=4, column=0)
 bot = Button(ventana, width=15, text=8, command= lambda: cargar(8))
@@ -77,22 +149,27 @@ bot.grid(padx=5, pady=5, row=4, column=1)
 bot = Button(ventana, width=15, text=9, command= lambda: cargar(9))
 bot.grid(padx=5, pady=5, row=4, column=2)
 
-"""
-num=1    
-for fila in range(2,5):
-    for columna in range(0,3):
-        bot = Button(ventana, width=15, text=f"{num}", command= lambda: cargar(num))
-        bot.grid(padx=5, pady=5, row=fila, column=columna)
-        num +=1
-"""
+#FILA BOTONES 4
 botSuma=Button(ventana, text="+", command= lambda: sumar() , width=15)
 botSuma.grid(padx=5, pady=5, row=5, column=0)
-
 bot = Button(ventana, width=15, text=0, command= lambda: cargar(0))
 bot.grid(padx=5, pady=5, row=5, column=1)
-
 botResta=Button(ventana, text="-", command= lambda: resta() , width=15)
 botResta.grid(padx=5, pady=5, row=5, column=2)
+
+#FILA BOTONES 5
+botResultado=Button(ventana, text="x", command= lambda: multiplicacion() , width=15)
+botResultado.grid(padx=5, pady=5, row=6, column=0)
+
+botResultado=Button(ventana, text="/", command= lambda: division() , width=15)
+botResultado.grid(padx=5, pady=5, row=6, column=1)
+
+botResultado=Button(ventana, text="=", command= lambda: resultadoOperacion() , width=15)
+botResultado.grid(padx=5, pady=5, row=6, column=2)
+
+
+botResultado=Button(ventana, text="Borrar", command= lambda: limpiar() , width=15)
+botResultado.grid(padx=5, pady=5, row=7, column=1)
 
 
  
